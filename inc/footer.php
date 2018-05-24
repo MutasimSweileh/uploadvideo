12f6a1d56b7d8cd8e24bd087c3b796b5878cb9c6
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-05-25 00:36:10 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -41,10 +41,11 @@
                           </div>
                         </div>
     </div>
-        <script src="js/jquery-2.1.4.js"></script>
+        <script src="http://hlstester.com/assets/js/jquery-3.2.1.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/menu.js"></script>
         <script src="js/plagn.js"></script>
+        <script async src="https://content.jwplatform.com/libraries/USNaSatu.js"></script>
        <!-- <script src="js/flowplayer.min.js"></script>-->
        <script src="https://releases.flowplayer.org/7.2.6/flowplayer.min.js"></script>
         <script src="//releases.flowplayer.org/hlsjs/flowplayer.hlsjs.light.min.js"></script>
@@ -99,5 +100,46 @@
         }
         });
         </script>
+<script>
+$(document).ready(function(e){
+    $("#stream_url").focus();$("#stream_url").on("focus",function(){$("#stream_url").select();});
+    setPlayerContainer();
+    $(".player-inner").click(function(e){loadPlayer();});});$(window).resize(function(e){setPlayerContainer();});
+    function setPlayerContainer()
+{var pc_width=$("#player-container").width();var pwidth=720;var pheight=423;if(pc_width<720)
+{var pwidth=pc_width;var pheight=pwidth*423/720;}
+$("#player").width(pwidth);$("#player").height(pheight);}
+function loadPlayer()
+{
+var url=$("#stream_url").val();
+var player_type=$("#player_type").val();
+if(validateUrl(url)){
+    var pwidth=$("#player").width();
+    var pheight=$("#player").height();
+    jwplayer("player").setup({file:url,width:pwidth,height:pheight,primary:'flash',autostart:'true',controlbar:"bottom",startparam:"start",});}}
+function isRTMP(url)
+{url_arr=url.split(":");if(url_arr[0].toLowerCase()=="rtmp")
+{return true}
+return false;}
+function validateUrl(url)
+{if(url=="")
+{$("#url-form").removeClass("has-success");$("#url-form").addClass("has-error");$("#url-form").addClass("has-feedback");var helperIcon='<span class="glyphicon glyphicon-warning-sign form-control-feedback" id="helper_icon"></span>';if($("#url-form").find("#helper_icon"))
+{$("#url-form").find("#helper_icon").remove();$("#url-form").append(helperIcon);}
+else
+{$("#url-form").append(helperIcon);}
+var errorText='<span class="help-block text-right" id="error_text">Please enter URL before play it.</span>';
+if($("#url-form").find("#error_text"))
+{$("#url-form").find("#error_text").remove();$("#url-form").append(errorText);}
+else
+{$("#url-form").append(errorText);}
+return false;}
+else
+{$("#url-form").removeClass("has-error");$("#url-form").addClass("has-success");$("#url-form").addClass("has-feedback");var helperIcon='<span class="glyphicon glyphicon-ok form-control-feedback" id="helper_icon"></span>';if($("#url-form").find("#helper_icon"))
+{$("#url-form").find("#helper_icon").remove();$("#url-form").append(helperIcon);}
+else
+{$("#url-form").append(helperIcon);}
+if($("#url-form").find("#error_text"))
+{$("#url-form").find("#error_text").remove();}
+return true;}}</script>
   </body>
 </html>
